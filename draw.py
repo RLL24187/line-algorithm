3b66f098f4ae398465c03de73697356ff221f6da
rleongPd5 <rleong00@stuy.edu>
2020-02-10 21:32:49 -0500

    lots of variable corrections

--- draw.py
+++ draw.py
@@ -5,33 +5,33 @@ def draw_line( x0, y0, x1, y1, screen, color ):
         x1, x0 = x0, x1
         y1, y0 = y0, y1 # have to reverse y's too
 
-    Y = int(y0) - int(y1) #change in Y a.k.a. A
-    X = int(x0) - int(x1) #change in X a.k.a. -B
+    A = int(y0) - int(y1) #change in Y
+    B = int(x1) - int(x0) #negative change in X
     tempx = x0
     tempy = y0
 
-    if (X == 0): # check vertical line first to prevent 0 division
+    if (B == 0): # check vertical line first to prevent 0 division
         for y in range (y0, y1 + 1):
             plot(screen, color, tempx, y)
     else:
-        slope = float(Y / X)
+        slope = float(A / B * -1)
         if (slope == 0): # horizontal line
             for x in range (x0, x1 + 1):
                 plot(screen, color, x, tempy)
 
         # octant 1 and 5
         # print(slope)
-        A2 = 2 * Y
-        B2 = -2 * X
+        A2 = 2 * A
+        B2 = 2 * B
         if (slope <= 1 and slope > 0 ):
             print("octant 1 or 5")
-            d = (2 * A) + B
+            d = A2 + B
             while (tempx <= x1):
                 # print("A: " + str(A) + "| B: " + str(B))
                 # print("d: " + str(d) + " x: " +str(x) + " y: " + str(y))
-                plot (screen, color, x, y)
+                plot (screen, color, tempx, tempy)
                 if (d > 0):
                     tempy = tempy + 1
-                    d = d + (2 * B)
+                    d = d + B2
                     tempx = tempx + 1
-                    d = d + (2 * A)
+                    d = d + A2
