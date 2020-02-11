510cb43d3ed3cd344d6d3edd2fc40eb60085c326
rleongPd5 <rleong00@stuy.edu>
2020-02-10 21:17:07 -0500

    fixed param naming issue

--- draw.py
+++ draw.py
@@ -7,17 +7,17 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 
     Y = int(y0) - int(y1) #change in Y a.k.a. A
     X = int(x0) - int(x0) #change in X a.k.a. -B
-    x = x0
-    y = y0
+    tempx = x0
+    tempy = y0
 
     if (X == 0): # check vertical line first to prevent 0 division
         for y in range (y0, y1 + 1):
-            plot(screen, color, x, y)
+            plot(screen, color, tempx, tempy)
     else:
         slope = float(Y / X)
         if (slope == 0): # horizontal line
             for x in range (x0, x1 + 1):
-                plot(screen, color, x, y)
+                plot(screen, color, tempx, tempy)
 
         # octant 1 and 5
         # print(slope)
@@ -26,12 +26,12 @@ def draw_line( x0, y0, x1, y1, screen, color ):
         if (slope <= 1 and slope > 0 ):
             print("octant 1 or 5")
             d = (2 * A) + B
-            while (x <= x1):
+            while (tempx <= x1):
                 # print("A: " + str(A) + "| B: " + str(B))
                 # print("d: " + str(d) + " x: " +str(x) + " y: " + str(y))
                 plot (screen, color, x, y)
                 if (d > 0):
-                    y = y + 1
+                    tempy = tempy + 1
                     d = d + (2 * B)
-                    x = x + 1
+                    tempx = tempx + 1
                     d = d + (2 * A)
