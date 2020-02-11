caaae4b9b3aed3fba0ed926e40a50ae6849dca99
rleongPd5 <rleong00@stuy.edu>
2020-02-10 21:14:52 -0500

    trying new approach to octant 1 and 5

--- draw.py
+++ draw.py
@@ -1,25 +1,37 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    A = int(y0) - int(y1) #change in Y
-    B = int(x0) - int(x0) #change in X
-    # slope = float(A / B)
-    # # octant 1 and 5
-    # print(slope)
-    # if (slope <= 1 and slope > 0 ):
-    print("octant 1 or 5")
-    oct1(1, 1, x0, y0, x1, y1, screen, color)
+    if (x1 < x0): # x1 should always be >= x0
+        x1, x0 = x0, x1
+        y1, y0 = y0, y1 # have to reverse y's too
 
-def oct1(A, B, x0, y0, x1, y1, screen, color):
+    Y = int(y0) - int(y1) #change in Y a.k.a. A
+    X = int(x0) - int(x0) #change in X a.k.a. -B
     x = x0
     y = y0
-    d = (2 * A) + B
-    while (x <= x1):
-        # print("A: " + str(A) + "| B: " + str(B))
-        # print("d: " + str(d) + " x: " +str(x) + " y: " + str(y))
-        plot (screen, color, x, y)
-        if (d > 0):
-            y = y + 1
-            d = d + (2 * B)
-        x = x + 1
-        d = d + (2 * A)
+
+    if (X == 0): # check vertical line first to prevent 0 division
+        for y in range (y0, y1 + 1):
+            plot(screen, color, x, y)
+    else:
+        slope = float(Y / X)
+        if (slope == 0): # horizontal line
+            for x in range (x0, x1 + 1):
+                plot(screen, color, x, y)
+
+        # octant 1 and 5
+        # print(slope)
+        A2 = 2 * Y
+        B2 = -2 * X
+        if (slope <= 1 and slope > 0 ):
+            print("octant 1 or 5")
+            d = (2 * A) + B
+            while (x <= x1):
+                # print("A: " + str(A) + "| B: " + str(B))
+                # print("d: " + str(d) + " x: " +str(x) + " y: " + str(y))
+                plot (screen, color, x, y)
+                if (d > 0):
+                    y = y + 1
+                    d = d + (2 * B)
+                    x = x + 1
+                    d = d + (2 * A)
