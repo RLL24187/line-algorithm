63b647dc6827f4d140264e60aa3af126d67becef
rleongPd5 <rleong00@stuy.edu>
2020-02-10 21:54:01 -0500

    trying out more stuff...

--- draw.py
+++ draw.py
@@ -7,32 +7,31 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 
     A = int(y0) - int(y1) #change in Y
     B = int(x1) - int(x0) #negative change in X
-    tempx = int(x0)
-    tempy = int(y0)
 
     if (B == 0): # check vertical line first to prevent 0 division
         for y in range (y0, y1 + 1):
-            plot(screen, color, tempx, y)
+            plot(screen, color, x, y)
     else:
+        print("A: " + A + " B: " + B)
         slope = float(A / -B)
         print(slope)
         if (slope == 0): # horizontal line
             for x in range (x0, x1 + 1):
-                plot(screen, color, x, tempy)
+                plot(screen, color, x, y)
 
         # octant 1 and 5
         # print(slope)
+        x = int(x0)
+        y = int(y0)
         A2 = 2 * A
         B2 = 2 * B
         if (slope <= 1 and slope > 0 ):
             print("octant 1 or 5")
             d = A2 + B
-            while (tempx <= x1):
-                # print("A: " + str(A) + "| B: " + str(B))
-                # print("d: " + str(d) + " x: " +str(x) + " y: " + str(y))
-                plot (screen, color, tempx, tempy)
+            while (x <= x1):
+                plot (screen, color, x, y)
                 if (d > 0):
-                    tempy = tempy + 1
+                    y = y + 1
                     d = d + B2
-                tempx = tempx + 1
+                x = x + 1
                 d = d + A2
