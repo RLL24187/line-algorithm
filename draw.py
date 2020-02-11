024789a534b88740a5c2aa1b9a4d0698ad182aaa
rleongPd5 <rleong00@stuy.edu>
2020-02-11 03:32:40 -0500

    testing octant 3 and 7

--- draw.py
+++ draw.py
@@ -46,3 +46,13 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                     d = d + A2
                 y = y + 1
                 d = d + B2
+        # octant 3 and 7
+        elif (slope >= -1 and slope < 0): #octant 8
+            d = A2 + B
+            while x <= x1:
+                plot (screen, color, x, y)
+                if d < 0:
+                    y = y - 1
+                    d = d - B2
+                x = x + 1
+                d = d + A2
